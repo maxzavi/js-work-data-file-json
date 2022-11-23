f4f4a745ceecf30f3f564050ccf0c30f0f5dbedd
MaxZ <mzavaletav@gmail.com>
2022-11-22 21:52:31 -0500

    feat: add table

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
-import { betFindById } from "../repository/bet-repo.js";
+import { betFindById, usersAll } from "../repository/bet-repo.js";
 
 document.getElementById('btn-find').addEventListener("click" ,evt=>{
     const id = document.getElementById('txt-val').value;
     const bet = betFindById(id);
     document.getElementById('sp-data').innerHTML= `<pre>${JSON.stringify(bet,null,2)}</pre>`;  
 });
+
+//Load table
+
+const users = usersAll();
+
+let content ='';
+let id=0;
+users.forEach(user=>{
+    content+=`
+        <tr>
+            <td>${id+=1}</td>
+            <td>${user.userName}</td>
+            <td>${user.sumGoles1}</td>
+        </tr>`;
+});
+const tableId=document.getElementById('tbl-users');
+const tBody = tableId.getElementsByTagName('tbody')[0].innerHTML=content;
+
+//console.log(tBody)
--- repository/bet-repo.js
+++ repository/bet-repo.js
@@ -3,3 +3,32 @@ import { bets } from "../database/db-bet.js";
 export const betFindById = (id) => {
     return bets.find(element => element.id == id);
 };
+
+export const usersAll = ()=>{
+    const users =[];
+    let userName='';
+    let gol1=0;
+    bets.forEach(bet=>{
+        if (userName!=bet.Participante){
+            if (userName!=''){
+                users.push ({
+                    userName:userName,
+                    sumGoles1:gol1
+                });                
+            }
+            /*
+            users.push ({
+                userName:bet.Participante,
+                sumGoles1:gol1
+            });*/
+            userName=bet.Participante;
+            gol1=0;
+        }
+        gol1+=bet.Goles1;
+    });
+    users.push ({
+        userName:userName,
+        sumGoles1:gol1
+    });  
+    return users;
+}
